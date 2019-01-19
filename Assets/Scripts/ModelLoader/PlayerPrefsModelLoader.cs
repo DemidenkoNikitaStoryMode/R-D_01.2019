c9dd6a63e7d175fc94a6906d4960fe981a832899
Rostislav Nikishin <littleponyapps@gmail.com>
2019-01-19 12:41:55 +0300

    Added playerprefs model loader. Added view and controller for builder

---
+++ Assets/Scripts/ModelLoader/PlayerPrefsModelLoader.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Newtonsoft.Json;
+using UnityEngine;
+
+namespace Assets.Scripts.ModelLoader
+{
+    public class PlayerPrefsModelLoader : IGameModelLoader
+    {
+        private const string GameModelKey = "gameState";
+
+        public GameModel LoadGameModel()
+        {
+            var serializedGameModel = PlayerPrefs.GetString(GameModelKey);
+            try
+            {
+                return JsonConvert.DeserializeObject<GameModel>(serializedGameModel);
+            }
+            catch(Exception exception)
+            {
+                throw new CantLoadModel($"Can't deserialize JSON model. Exception: {exception}");
+            }
+        }
+
+        public bool SaveGameModel(GameModel model)
+        {
+            var serializedGameModel = JsonConvert.SerializeObject(model);
+            PlayerPrefs.SetString(GameModelKey, serializedGameModel);
+            return true;
+        }
+    }
+}
