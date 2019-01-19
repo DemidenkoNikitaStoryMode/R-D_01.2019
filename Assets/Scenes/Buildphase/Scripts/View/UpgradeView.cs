d25d30dd30511e6c703070d36688d20b7eecd1c4
Demidenko Nikita <demidenko@storymode.pro>
2019-01-19 14:37:43 +0300

    Builder scene. View updates.

--- Assets/Scenes/Buildphase/Scripts/View/UpgradeView.cs
+++ Assets/Scenes/Buildphase/Scripts/View/UpgradeView.cs
@@ -18,75 +18,36 @@ namespace Assets.Scenes.Buildphase.Scripts.View
 
         private void Start()
         {
-            if (Damage)            
-                Damage.upgradeStat.onClick.AddListener(() => UpgradeAction(UpgradeType.Damage));
-            
 
-            if (Health)          
-                Health.upgradeStat.onClick.AddListener(() => UpgradeAction(UpgradeType.Health));
-            
-
-            if (FireRate)
-                Damage.upgradeStat.onClick.AddListener(() => UpgradeAction(UpgradeType.FireRate));
-            
-
-            if (SlowDownFactor)
-                Damage.upgradeStat.onClick.AddListener(() => UpgradeAction(UpgradeType.SlowDown));
-            
+            Damage.upgradeStat.onClick.AddListener(() => UpgradeAction(UpgradeType.Damage));
+            Health.upgradeStat.onClick.AddListener(() => UpgradeAction(UpgradeType.Health));
+            FireRate.upgradeStat.onClick.AddListener(() => UpgradeAction(UpgradeType.FireRate));
+            SlowDownFactor.upgradeStat.onClick.AddListener(() => UpgradeAction(UpgradeType.SlowDown));
+            //Money
         }
 
         public void SetDamage(int min, int max)
         {
-            if (!Damage)
-            {
-                Debug.Log("Damage==null");
-                return;
-            }
-
             Damage.value.text = min + " ~ " + max;
         }
 
         public void SetHealth(int health)
         {
-            if (!Health)
-            {
-                Debug.Log("Health==null");
-                return;
-            }
-
             Health.value.text = health.ToString();
         }
 
         public void SetFireRate(float fireRate)
         {
-            if (!FireRate)
-            {
-                Debug.Log("FireRate==null");
-                return;
-            }
-
             FireRate.value.text = fireRate.ToString();
         }
 
         public void SetSlowDownFactor(float slowDownPercent)
         {
-            if (!SlowDownFactor)
-            {
-                Debug.Log("SlowDownFactor==null");
-                return;
-            }
-
             SlowDownFactor.value.text = slowDownPercent.ToString() + "%";
         }
 
         public void SetMoney(int money)
         {
-            if (!Money)
-            {
-                Debug.Log("Money==null");
-                return;
-            }
-
             Money.value.text = money.ToString();
         }
     }
