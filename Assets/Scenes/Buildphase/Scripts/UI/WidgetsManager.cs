bc635b88852fe107d2b734190ee5f02aea7d49a3
Demidenko Nikita <demidenko@storymode.pro>
2019-01-19 15:11:20 +0300

    Builder scene update.

---
+++ Assets/Scenes/Buildphase/Scripts/UI/WidgetsManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WidgetsManager : MonoBehaviour
+{
+
+
+
+    private void Start()
+    {
+        
+    }
+
+    /*private T ShowWidget<T>() where T : BaseWidget
+    {
+        T window = null;
+
+        for (int i = 0; i < GUIWindows.Count; i++)
+        {
+            if (GUIWindows[i].GetType() == typeof(T))
+            {
+                window = (T)GUIWindows[i];
+            }
+        }
+
+        //return window;
+    }*/
+}
