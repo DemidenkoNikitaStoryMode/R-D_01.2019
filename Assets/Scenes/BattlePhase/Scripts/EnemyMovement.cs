dcdf6bce00165b2fcb6eab909316dd67e7ad358b
Dmitry Sheremet <sheremet@storymode.pro>
2019-01-19 14:01:11 +0300

    Enemy created

---
+++ Assets/Scenes/BattlePhase/Scripts/EnemyMovement.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+
+    public float Speed = 1;
+    public Rigidbody2D Rb;
+
+    private bool shouldMove = true;
+
+    private void Start()
+    {
+        transform.up = Tower.Instance.transform.position - transform.position;
+    }
+
+    private void FixedUpdate()
+    {
+        if (!shouldMove)
+        {
+            Rb.velocity = Vector2.zero;
+            return;
+        }
+        Rb.velocity = transform.up * Speed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.CompareTag("Tower"))
+        {
+            shouldMove = false;
+
+        }
+    }
+}
